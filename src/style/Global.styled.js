92cf4b9b6c71d91c5dcbf1308579c1eb932987bc
wyliemickelson <wyliecoyote910@gmail.com>
2022-09-21 17:36:49 -0700

    added initial structure

---
+++ src/style/Global.styled.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    scroll-behavior: smooth;
+  }
+
+  img {
+    max-width: 100%;
+  }
+`
